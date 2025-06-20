c26acda66e36c7b33f3670529c221ceaf233e9a0
Andriy <114863893+noinsts@users.noreply.github.com>
2025-06-20 23:59:58 +0300

    Add SinSum.java

---
+++ src/SinSum.java
@@ -0,0 +1,25 @@
+/*
+https://basecamp.eolymp.com/uk/problems/11433
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class SinSum {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader sc = new BufferedReader(new InputStreamReader(System.in))) {
+            int x = Integer.parseInt(sc.readLine());
+
+            double sum = 0.0;
+            double currSin = 0.0;
+
+            for (int i = 1; i <= x; i++) {
+                currSin = Math.sin(i);
+                sum += 1 / currSin;
+            }
+
+            System.out.printf("%.6f%n", sum);
+        }
+    }
+}
