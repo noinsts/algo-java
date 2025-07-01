0598d414569abe213f7a7d716d94467e5e4f36e0
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-02 00:09:40 +0300

    Update Divide369.java

--- src/Divide369.java
+++ src/Divide369.java
@@ -1,16 +1,26 @@
+/*
+https://basecamp.eolymp.com/uk/problems/4734
+ */
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
+import java.math.BigInteger;
 
 
 public class Divide369 {
     public static void main(String[] args) throws IOException {
         try (BufferedReader sc = new BufferedReader(new InputStreamReader(System.in))) {
-            int x = Integer.parseInt(sc.readLine());
+            BigInteger x = new BigInteger(sc.readLine());
 
-            System.out.println(x % 3 == 0 ? "Yes" : "No");
-            System.out.println(x % 6 == 0 ? "Yes" : "No");
-            System.out.println(x % 9 == 0 ? "Yes" : "No");
+            System.out.println(checker(x, 3) ? "Yes" : "No");
+            System.out.println(checker(x, 6) ? "Yes" : "No");
+            System.out.println(checker(x, 9) ? "Yes" : "No");
         }
+    }
+
+    private static boolean checker(BigInteger x, long y) {
+        return x.mod(BigInteger.valueOf(y)).equals(BigInteger.ZERO);
     }
 }
