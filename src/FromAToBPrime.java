3e4fe7427b0c6241dfff9ed6f9f414af28ff5e50
Andriy <114863893+noinsts@users.noreply.github.com>
2025-07-05 23:57:02 +0300

    Update FromAToBPrime.java

--- src/FromAToBPrime.java
+++ src/FromAToBPrime.java
@@ -1,35 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class FromAToBPrime {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader sc = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] line = sc.readLine().split(" ");
 
-        long a = sc.nextLong();
-        long b = sc.nextLong();
+            int a = Integer.parseInt(line[0]);
+            int b = Integer.parseInt(line[1]);
 
-        long counter = 0;
+            boolean found = false;
 
-        for (long i = a; i < b+1; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-                counter++;
-            }
-        }
-
-        if (counter == 0) {
-            System.out.println("Absent");
+            for (int i = a; i <= b; i++) {
+               if (isPrime(i)) {
+                   found = true;
+                   System.out.println(i);
+               }
+           }
+           if (!found) {
+               System.out.println("Absent");
+           }
         }
     }
 
-    public static boolean isPrime(long number) {
-        if (number < 2) {
-            return false;
-        }
+    private static boolean isPrime(int n) {
+        if (n <= 1) return false;   // 0,1 — не прості
+        if (n == 2) return true;    // 2 — просте
+        if (n % 2 == 0) return false; // всі парні крім 2 — не прості
 
-        for (long i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-            }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) return false;
         }
         return true;
     }
